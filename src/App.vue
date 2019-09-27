db79fecc9f13de864bb09ea418c8955d0f46ad0f
yaojunhao <yaojunhao@dongfangfuli.com>
2019-09-27 13:43:16 +0800

    根据设备判断跳转pc还是h5

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,30 @@
   </div>
 </template>
 
+<script>
+import { getQueryString, detect } from "@/assets/utils";
+
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this.redirect();
+  },
+  methods: {
+    redirect() {
+      let union = getQueryString("union");
+      let city = getQueryString("city");
+      let ua = navigator.userAgent;
+      // console.log(detect(ua));
+      if (!detect(ua).os.phone) {
+        window.location = `http://${union}.dongfangfuli.com/bfd_pc/index?union=${union}&city=${city}`;
+      }
+    }
+  }
+};
+</script>
+
 <style lang="less">
 @import "./assets/styles/base/reset.less";
 @import "./assets/styles/fonts/index.less";
