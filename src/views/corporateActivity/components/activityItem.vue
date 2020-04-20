74c7b6b08cec025f20a7562bbb634cb584ba9e76
xiexueli <1030483963@qq.com>
2020-04-20 17:35:41 +0800

    企业活动列表页

---
+++ src/views/corporateActivity/components/activityItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <section class="activity-wrap">
+        <div class="activit-item" v-for="(item,index) in data" :key="index">
+            <div class="activity-pic">
+                <img :src="item.pic" alt="">
+            </div>
+            <div class="activity-text">
+                <div class="activity-desc">{{item.title}}</div>
+                <div class="activity-status">{{item.statusDesc}}</div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: "activityItem",
+    props:{
+      data: {
+        type: Array,
+        default: function () {
+          return []
+        }
+      },
+    },
+  }
+</script>
+
+<style lang="less" scoped>
+.activity-wrap{
+    .activit-item{
+        width: 100%;
+        height:200px;
+        .activity-pic{
+            width: 100%;
+            height:150px;
+            img{
+                width: 100%;
+                height: 100%;
+                display: block;
+            }
+        }
+        .activity-text{
+            display: flex;
+            justify-content:space-around;
+            height: 50px;
+            line-height: 50px;
+            .activity-desc{
+                width: 80%;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+            }
+            .activity-status{
+                width: 20%;
+                text-align: right;
+            }
+        }
+    }
+
+}
+</style>
--- src/views/corporateActivity/index.vue
+++ src/views/corporateActivity/index.vue
@@ -1,14 +1,25 @@
 <template>
-    <div @click="activityDetail">陪伴孩子成长，享受天真快乐</div>
+    <section class="page">
+        <Activity :data="list"></Activity>
+    </section>
 </template>
 
 <script>
+    import Activity from './components/activityItem'
 export default {
     data(){
         return {
-
+          list:[
+            {"pic":"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587385230756&di=366f4dfe738f6f9db88f374a2f9d1893&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20181105%2F20%2F1541422050-ZzanIJQjpv.jpg","title":"陪伴孩子成长，享受天真快乐","statusDesc":"报名中"},
+            {"pic":"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3257444195,3291178773&fm=26&gp=0.jpg","title":"用快乐搅拌，用甜蜜点缀","statusDesc":"即将开始"},
+            {"pic":"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=320899456,3455234350&fm=26&gp=0.jpg","title":"用快乐搅拌，用甜蜜点缀","statusDesc":"已报名"},
+            {"pic":"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=313099192,176112715&fm=26&gp=0.jpg","title":"用快乐搅拌，用甜蜜点缀","statusDesc":"已报名"},
+          ],
         }
     },
+  components:{
+    Activity,
+  },
     methods:{
         activityDetail(){
             this.$router.push({path:'/corporateactivity/activitydetail',query:''});
@@ -16,6 +27,9 @@ export default {
     }
 }
 </script>
-<style scoped>
-
+<style lang="less" scoped>
+.page{
+    padding: 0 10px;
+    font-size: 14px;
+}
 </style>
