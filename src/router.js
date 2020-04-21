b0260348b0446ebb94df63f3411651e3bcdb59dd
xiexueli <1030483963@qq.com>
2020-04-21 19:47:14 +0800

    首页路由加上h5标识

--- src/router.js
+++ src/router.js
@@ -30,7 +30,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "oldHome" */ "./views/oldHome")
     },
     {
-      path: "/home",
+      path: "/home-h5",
       name: "home",
       component:Home,
       meta: {
@@ -39,7 +39,7 @@ export default new Router({
     },
     {
       path: "/",
-      redirect: "/home"
+      redirect: "/home-h5"
     },
     {
       path: "/corporatenews",
