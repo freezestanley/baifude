37c3cb22e2ebb7adc3114debb3fb98f248818700
daibaoguang <dbg8685@gmail.com>
2020-05-06 17:49:40 +0800

    # 添加调研

--- src/views/staffSurvey/index.vue
+++ src/views/staffSurvey/index.vue
@@ -1,58 +1,112 @@
 <template>
-    <div class="page">
-         <NoticeList :data="list" @goToDetail="goToDetail"></NoticeList>
-    </div>
+  <div class="page">
+    <Banner :bannerList="bannerList" @clickBanner="clickBanner"></Banner>
+    <SurveyItem
+      :surveyData="listObj"
+      @goToDetail="goToDetail"
+      @onLoad="onLoad"
+      @onRefresh="onRefresh"
+    />
+  </div>
 </template>
 
 <script>
-  import NoticeList from '@/components/noticeList/index';
-  export default {
-    name: "index",
-    components:{
-      NoticeList,
-    },
-    data(){
-      return{
-        list:[
-          {"title":"春节假期去向调查表","time":"2020-04-13"},
-          {"title":"企业员工返岗调查表","time":"2020-04-13"},
-          {"title":"员工每日健康上报","time":"2020-04-13"},
-          {"title":"疫情特殊时期，在家工作临时方案办公环境调查","time":"2020-04-13"},
-          {"title":"2019年度优秀员工名单","time":"2020-04-13"},
-          {"title":"2019年度优秀员工名单","time":"2020-04-13"},
-        ],
+import Banner from "./components/banner";
+import SurveyItem from "./components/surveyItem";
+import { cms_researchList } from "@/assets/apis/home";
+import utilRes from "@/assets/utils/resResult";
+
+export default {
+  name: "index",
+  components: {
+    Banner,
+    SurveyItem
+  },
+  async created() {},
+  data() {
+    return {
+      bannerList: [
+        {
+          picture:
+            "http://devimg.dongfangfuli.com/bfd/2020-05-02/276d7252c2cdf98f2fcafb58e902ac91.png"
+        }
+      ],
+      // list: [],
+      listObj: {
+        list: [],
         loading: false,
         finished: false,
+        refreshing: false
+      },
+      currentPage: 1,
+      itemsPerPage: 12,
+      total: 0
+    };
+  },
+  methods: {
+    async onLoad(fn = "onLoad") {
+      if (this.listObj.refreshing) {
+        this.listObj.list = [];
+        this.listObj.refreshing = false;
+      }
+      await this.queryResearchList(fn);
+      this.listObj.loading = false;
+    },
+    onRefresh() {
+      // 清空列表数据
+      this.listObj.finished = false;
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      this.listObj.loading = true;
+      this.currentPage = 1;
+      this.listObj.list = [];
+      this.onLoad("refresh");
+    },
+    // 查询List
+    async queryResearchList(fn) {
+      let params = {
+        currentPage: this.currentPage,
+        itemsPerPage: this.itemsPerPage,
+        status: 2
+      };
+      const obj = { ...params };
+      let res = await cms_researchList(obj);
+      if (utilRes.successCheck(res)) {
+        if (fn === "onLoad") {
+          this.listObj.list = [...this.listObj.list, ...res.data.listObj]; //请求返回当页的列表
+        } else {
+          this.listObj.list = res.data.listObj; //请求返回当页的列表
+        }
+        this.currentPage++;
+        this.total = res.data.total;
+        // 没有数据关闭下拉
+        if (this.listObj.list.length >= this.total) {
+          this.listObj.finished = true;
+        }
+        this.listObj.loading = false;
+      } else {
+        this.$message({
+          type: "error",
+          message: res.errMsg ? res.errMsg : "调用接口失败!"
+        });
       }
     },
-    methods:{
-      // onLoad() {
-      //   // 异步更新数据
-      //   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      //   setTimeout(() => {
-      //     for (let i = 0; i < 5; i++) {
-      //       this.list.push({"title":"2019年度优秀员工名单","time":"2020-04-13"});
-      //     }
-      //
-      //     // 加载状态结束
-      //     this.loading = false;
-      //
-      //     // 数据全部加载完成
-      //     if (this.list.length >= 20) {
-      //       this.finished = true;
-      //     }
-      //   }, 1000);
-      // },
-      goToDetail(item){
-        console.log("item111",item)
-      }
+    clickBanner(v) {
+      console.log(v);
     },
+    goToDetail(item) {
+      this.$router.push({
+        name: "staffDetail",
+        params: { id: item.id }
+      });
+    }
   }
+};
 </script>
 
 <style lang="less" scoped>
-    .page{
-        padding: 0 10px;
-        font-size: 14px;
-    }
+.page {
+  padding: 0 10px;
+  font-size: 14px;
+}
 </style>
