d7938f204cdfcac58ce62918b1fec431bb440d0d
张永禄 <zhangyonglu@dongfangfuli.com>
2020-05-09 01:48:52 +0800

    页面记住位置

--- src/router.js
+++ src/router.js
@@ -229,7 +229,17 @@ const routes = [
 ];
 const router = new Router({
   mode: "history",
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {
+        from.meta.savedPosition = document.body.scrollTop
+      }
+      return { x: 0, y: to.meta.savedPosition || 0 }
+    }
+  }
 });
 const checkLogin = async () => {
   const res = await user_checkLogin();
