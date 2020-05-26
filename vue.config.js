c7a26933a75757dbb9c4edd39ed6b7f1fe337d7c
张永禄 <zhangyonglu@dongfangfuli.com>
2020-05-26 13:50:19 +0800

    新增版本控制文件

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const { env } = process;
 const { DEV_SERVER } = require("./config");
 const fs = require("fs");
 const gitHEAD = fs.readFileSync('.git/HEAD', 'utf-8').trim(); // ref: refs/heads/develop
-let ref, gitVersion;
+let ref, gitVersion, cDate, branch;
 if(gitHEAD.match(/^[0-9a-z]+$/)){
   gitVersion = gitHEAD;
 }else{
@@ -15,9 +15,28 @@ if(gitHEAD.match(/^[0-9a-z]+$/)){
   gitVersion = fs.readFileSync('.git/' + ref, 'utf-8').trim(); // git版本号，例如：6ceb0ab5059d01fd444cf4e78467cc2dd1184a66
 }
 console.log('gitHEAD:', gitHEAD)
-console.log('ref:', ref);
-// const develop = gitHEAD.split('/')[2] // 环境：develop
-fs.writeFileSync('./public/version.html', gitVersion);
+// console.log('ref:', ref);
+
+fs.writeFileSync('./public/version.html', `<h3>Version: ${gitVersion}</h3>`);
+var exec = require('child_process').exec;
+var cmd1 = `cat ./.git/logs/HEAD | grep -E ${gitVersion} -m 1 | awk '{print $5}'`;
+var cmd2 = `git name-rev --name-only HEAD`;
+exec(cmd1, function(error, stdout, stderr){
+  const ts = stdout.trim()+'000'-0;
+  const tmpDate = new Date(ts);
+  const y = tmpDate.getFullYear(), M = tmpDate.getMonth() + 1, d = tmpDate.getDate(), H = tmpDate.getHours(),
+    m = tmpDate.getMinutes(), s = tmpDate.getSeconds();
+  cDate = `${y}-${M}-${d} ${H}:${m}:${s}`;
+  // console.log('输出:', d, cDate);
+  const outputStr = `<h3>CommitTime: ${cDate}</h3>`;
+  fs.appendFileSync('./public/version.html', outputStr);
+});
+exec(cmd2,function(error, stdout, stderr){
+  const outputStr = `<h3>TagName: ${stdout.replace('tags/','')}</h3>`;
+  fs.appendFileSync('./public/version.html', outputStr);
+});
+
+
 
 module.exports = {
   publicPath: env.NODE_ENV === "development" ? "/" : `${env.publicPath}/home-h5/`,
