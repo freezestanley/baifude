d99e7b4bae53e271d6f82b6a36ec77801bb4f4c9
xiexueli <1030483963@qq.com>
2020-06-11 15:25:05 +0800

    时尚风

--- src/views/welfareMall/fashion/index.vue
+++ src/views/welfareMall/fashion/index.vue
@@ -2,18 +2,48 @@
     <div>
         <div>fashion</div>
         <navArea></navArea>
+        <component
+            :is="ford.comp"
+            :content="ford.data"
+            v-for="(ford, index) of fords"
+            :key="index"
+        ></component>
     </div>
     
 </template>
 <script>
-import navArea from './components/navArea'
+  import { mapState } from "vuex";
+  import navArea from './components/navArea'
 export default {
     data(){
-        return {};
+        return {
+          fords:[],
+        };
+    },
+    created(){
+        this.getData()
     },
     components: {
         navArea
     },
+    methods:{
+        getData(){
+          if(this.unionAndPlatform){
+            this.unionAndPlatform.forEach(unionMod => {
+              this.fords.push({
+                data: unionMod,
+                comp: () => import(`@/components/${unionMod.formatCode}`)
+              });
+            });
+          }
+        }
+      },
+    computed: {
+        ...mapState({
+          unionAndPlatform:state => state.unionAndPlatform,//十大模块
+        }),
+
+      },
     
 }
 </script>
