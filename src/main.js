30c4a93a6e4c7adcd342f35a2e8271faa08ba5f5
machao01 <machao01@dongfangfuli.com>
2020-07-09 15:58:54 +0800

    优化点击延迟300

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import "amfe-flexible";
 import VueScroller from 'vue-scroller';
 import * as apis from "./assets/apis";
 import VConsole from 'vconsole';
+import FastClick from 'fastclick';
+
+FastClick.prototype.focus = function (targetElement) {
+  let length;
+  if (targetElement.setSelectionRange && targetElement.type.indexOf('date') !== 0 && targetElement.type !== 'time' && targetElement.type !== 'month') {
+      length = targetElement.value.length;
+      targetElement.focus();
+      targetElement.setSelectionRange(length, length);
+  } else {
+      targetElement.focus();
+  }
+};
+
+FastClick.attach(document.body)
 
 Vue.prototype.apis = apis;
 
