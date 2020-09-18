921e755a234464541b95248e2dd1d2400e852f4f
xiexueli <1030483963@qq.com>
2020-09-18 11:28:18 +0800

    首页公共接口调两遍优化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -313,7 +313,7 @@ export default {
     // this.unionConfigMess = JSON.parse(sessionStorage.getItem("unionConf"));
     const openAggregation = true;
     const union = getQueryString("union");
-    this.getunionConfig();//首页去除调两遍公共接口换算方法
+    this.getunionConfigData();//首页去除调两遍公共接口换算方法
     // this.isLogin(union);
     // this.getNotice(union);
     // this.getData(union);
@@ -530,7 +530,7 @@ export default {
         });
       }
     },
-    getunionConfig(){
+    getunionConfigData(){
       if (this.unionConf && this.unionConf.body) {
         let unionConf = this.unionConf;
         this.unionConfigMess = unionConf.body.unionConfigurationDto;
